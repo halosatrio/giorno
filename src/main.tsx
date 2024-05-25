9c7190a74b7fe8a60e80aa7a47b6a3c013cc8cc3
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-05-26 05:21:01 +0700

    another commit

--- src/main.tsx
+++ src/main.tsx
@@ -16,12 +16,8 @@ declare module "@tanstack/react-router" {
 }
 
 // Render the app
-const rootElement = document.getElementById("root")!;
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
